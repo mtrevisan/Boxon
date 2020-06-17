ade4afd3e9cbb2e276d75e8f39d468a1652825f8
Mauro Trevisan <mauro.simioni@targaubiest.com>
2020-06-17 17:58:53 +0200

    no message

--- src/main/java/unit731/boxon/codecs/ByteOrder.java
+++ src/main/java/unit731/boxon/codecs/ByteOrder.java
@@ -30,9 +30,4 @@ public enum ByteOrder{
 	LITTLE_ENDIAN,
 	BIG_ENDIAN;
 
-
-	@SuppressWarnings("unused")
-	public static final ByteOrder NATIVE_BYTE_ORDER = (java.nio.ByteOrder.nativeOrder() == java.nio.ByteOrder.BIG_ENDIAN?
-		BIG_ENDIAN: LITTLE_ENDIAN);
-
 }
