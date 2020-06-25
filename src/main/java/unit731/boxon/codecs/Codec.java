8fa5bc30489acca0b38dea5bf64dd76b5e07231f
Mauro Trevisan <mauro.simioni@targaubiest.com>
2020-06-25 17:27:52 +0200

    no message

--- src/main/java/unit731/boxon/codecs/Codec.java
+++ src/main/java/unit731/boxon/codecs/Codec.java
@@ -34,11 +34,9 @@ import unit731.boxon.helpers.AnnotationHelper;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 import java.util.StringJoiner;
-import java.util.stream.Collectors;
 
 
 /**
