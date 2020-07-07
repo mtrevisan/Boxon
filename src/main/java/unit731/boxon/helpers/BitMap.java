91e7d792289acfd6ae3a1131322eb414d86e6c77
Mauro Trevisan <mauro.simioni@targaubiest.com>
2020-07-07 17:59:56 +0200

    no message

--- src/main/java/unit731/boxon/helpers/BitMap.java
+++ src/main/java/unit731/boxon/helpers/BitMap.java
@@ -5,6 +5,12 @@ import java.util.Arrays;
 import java.util.BitSet;
 
 
+/**
+ * @see <a href="https://github.com/RoaringBitmap">Roaring Bitmap</a>
+ * https://github.com/lemire/sparsebitmap/blob/master/src/main/java/sparsebitmap/SparseBitmap.java
+ * https://github.com/brettwooldridge/SparseBitSet/blob/master/src/main/java/com/zaxxer/sparsebits/SparseBitSet.java
+ * https://lucene.apache.org/core/3_0_3/api/core/org/apache/lucene/util/OpenBitSet.html
+ */
 public class BitMap{
 
 	private BitSet bset;
@@ -53,13 +59,37 @@ public class BitMap{
 		return new BitMap(Arrays.copyOf(array, n));
 	}
 
-	public static BitMap valueOf(final ByteBuffer bb){
+	/**
+	 * Returns a new bit set containing all the bits in the given byte
+	 * buffer between its position and limit.
+	 *
+	 * <p>More precisely,
+	 * <br>{@code BitSet.valueOf(bb).get(n) == ((bb.get(bb.position()+n/8) & (1<<(n%8))) != 0)}
+	 * <br>for all {@code n < 8 * bb.remaining()}.
+	 *
+	 * <p>The byte buffer is not modified by this method, and no
+	 * reference to the buffer is retained by the bit set.
+	 *
+	 * @param buffer	A byte buffer containing a little-endian representation
+	 * 	of a sequence of bits between its position and limit, to be
+	 * 	used as the initial bits of the new bit set
+	 * @return	A {@code BitMap} containing all the bits in the buffer in the specified range
+	 */
+	public static BitMap valueOf(final ByteBuffer buffer){
 		//TODO
 		final BitMap bm = new BitMap(0);
-		bm.bset = BitSet.valueOf(bb);
+		bm.bset = BitSet.valueOf(buffer);
 		return bm;
 	}
 
+	/**
+	 * Creates a bit set whose initial size is large enough to explicitly
+	 * represent bits with indices in the range {@code 0} through
+	 * {@code nbits-1}. All bits are initially {@code false}.
+	 *
+	 * @param length	The initial size of the bit set
+	 * @throws NegativeArraySizeException	If the specified initial size is negative
+	 */
 	public BitMap(final int length){
 		//TODO
 		bset = new BitSet(length);
@@ -67,42 +97,113 @@ public class BitMap{
 
 	/**
 	 * Creates a bit set using words as the internal representation.
-	 * The last word (if there is one) must be non-zero.
+	 * <p>The last word (if there is one) must be non-zero.</p>
 	 */
 	private BitMap(final long[] array){
 		bset = BitSet.valueOf(array);
 	}
 
+	/**
+	 * Returns the value of the bit with the specified index. The value
+	 * is {@code true} if the bit with the index {@code bitIndex}
+	 * is currently set in this {@code BitSet}; otherwise, the result
+	 * is {@code false}.
+	 *
+	 * @param bitIndex	The bit index
+	 * @return	The value of the bit with the specified index
+	 * @throws IndexOutOfBoundsException	If the specified index is negative
+	 */
 	public boolean get(final int bitIndex){
 		//TODO
 		return bset.get(bitIndex);
 	}
 
+	/**
+	 * Sets the bit at the specified index to {@code true}.
+	 *
+	 * @param bitIndex	A bit index
+	 * @throws IndexOutOfBoundsException	If the specified index is negative
+	 */
 	public void set(final int bitIndex){
 		//TODO
 		bset.set(bitIndex);
 	}
 
+	/**
+	 * Sets the bit at the specified index to the complement of its current value.
+	 *
+	 * @param bitIndex	The index of the bit to flip
+	 * @throws IndexOutOfBoundsException	If the specified index is negative
+	 */
 	public void flip(final int bitIndex){
 		//TODO
 		bset.flip(bitIndex);
 	}
 
+	/**
+	 * Returns the index of the first bit that is set to {@code true}
+	 * that occurs on or after the specified starting index.
+	 * <p>If no such bit exists then {@code -1} is returned.</p>
+	 *
+	 * <p>To iterate over the {@code true} bits in a {@code BitMap},
+	 * use the following loop:
+	 *
+	 * <pre> {@code
+	 * for (int i = bs.nextSetBit(0); i >= 0; i = bs.nextSetBit(i+1)) {
+	 *     // operate on index i here
+	 *     if (i == Integer.MAX_VALUE) {
+	 *         break; // or (i+1) would overflow
+	 *     }
+	 * }}</pre>
+	 *
+	 * @param fromIndex	The index to start checking from (inclusive)
+	 * @return	The index of the next set bit, or {@code -1} if there is no such bit
+	 * @throws IndexOutOfBoundsException	If the specified index is negative
+	 */
 	public int nextSetBit(final int fromIndex){
 		//TODO
 		return bset.nextSetBit(fromIndex);
 	}
 
+	/**
+	 * Returns the "logical size" of this {@code BitMap}: the index of
+	 * the highest set bit in the {@code BitMap} plus one.
+	 * <p>Returns zero if the {@code BitMap} contains no set bits.</p>
+	 *
+	 * @return	The logical size of this {@code BitMap}
+	 */
 	public int length(){
 		//TODO
 		return bset.length();
 	}
 
+	/**
+	 * Returns a new byte array containing all the bits in this bit set.
+	 *
+	 * <p>More precisely, if
+	 * <br>{@code byte[] bytes = s.toByteArray();}
+	 * <br>then {@code bytes.length == (s.length()+7)/8} and
+	 * <br>{@code s.get(n) == ((bytes[n/8] & (1<<(n%8))) != 0)}
+	 * <br>for all {@code n < 8 * bytes.length}.
+	 *
+	 * @return	A byte array containing a little-endian representation of all the bits in this bit set
+	 */
 	public byte[] toByteArray(){
 		//TODO
 		return bset.toByteArray();
 	}
 
+	/**
+	 * Returns a new long array containing all the bits in this bit set.
+	 *
+	 * <p>More precisely, if
+	 * <br>{@code long[] longs = s.toLongArray();}
+	 * <br>then {@code longs.length == (s.length()+63)/64} and
+	 * <br>{@code s.get(n) == ((longs[n/64] & (1L<<(n%64))) != 0)}
+	 * <br>for all {@code n < 64 * longs.length}.
+	 *
+	 * @return	A long array containing a little-endian representation of all the bits in this bit set
+	 */
 	public long[] toLongArray(){
 		//TODO
 		return bset.toLongArray();
