d29873cb27bef24ee842508854b8c4a3f608b537
Mauro Trevisan <mauro.simioni@targaubiest.com>
2020-07-27 14:07:31 +0200

    no message

--- src/main/java/io/github/mtrevisan/boxon/helpers/Memoizer.java
+++ src/main/java/io/github/mtrevisan/boxon/helpers/Memoizer.java
@@ -39,26 +39,26 @@ public final class Memoizer{
 
 	private Memoizer(){}
 
-	public static <T> Supplier<T> memoize(final Supplier<T> supplier){
-		final Map<Object, T> cache = new ConcurrentHashMap<>();
+	public static <OUT> Supplier<OUT> memoize(final Supplier<OUT> supplier){
+		final Map<Object, OUT> cache = new ConcurrentHashMap<>();
 		return () -> cache.computeIfAbsent(DEFAULT_OBJECT, t -> supplier.get());
 	}
 
-	public static <T, U> Function<T, U> memoize(final Function<T, U> function){
-		final Map<T, U> cache = new ConcurrentHashMap<>();
+	public static <IN, OUT> Function<IN, OUT> memoize(final Function<IN, OUT> function){
+		final Map<IN, OUT> cache = new ConcurrentHashMap<>();
 		return input -> cache.computeIfAbsent(input, function);
 	}
 
 	/**
 	 * Thread-safe and recursion-safe implementation using a re-entrant lock
 	 *
-	 * @param <T>			Type of input to the function
+	 * @param <OUT>			Type of input to the function
 	 * @param supplier	The function to be memoized
 	 * @return				The new memoized function
 	 * @see <a href="https://opencredo.com/lambda-memoization-in-java-8/">Lambda memoization in Java 8</a>
 	 */
-	public static <T> Supplier<T> memoizeThreadAndRecursionSafe(final Supplier<T> supplier){
-		final Map<Object, T> cache = new HashMap<>(0);
+	public static <OUT> Supplier<OUT> memoizeThreadAndRecursionSafe(final Supplier<OUT> supplier){
+		final Map<Object, OUT> cache = new HashMap<>(0);
 		final ReentrantLock lock = new ReentrantLock();
 		return () -> {
 			lock.lock();
@@ -74,14 +74,14 @@ public final class Memoizer{
 	/**
 	 * Thread-safe and recursion-safe implementation using a re-entrant lock
 	 *
-	 * @param <T>			Type of input to the function
-	 * @param <U>			Type of output from the function
+	 * @param <IN>			Type of input to the function
+	 * @param <OUT>			Type of output from the function
 	 * @param function	The function to be memoized
 	 * @return				The new memoized function
 	 * @see <a href="https://opencredo.com/lambda-memoization-in-java-8/">Lambda memoization in Java 8</a>
 	 */
-	public static <T, U> Function<T, U> memoizeThreadAndRecursionSafe(final Function<T, U> function){
-		final Map<T, U> cache = new HashMap<>(0);
+	public static <IN, OUT> Function<IN, OUT> memoizeThreadAndRecursionSafe(final Function<IN, OUT> function){
+		final Map<IN, OUT> cache = new HashMap<>(0);
 		final ReentrantLock lock = new ReentrantLock();
 		return input -> {
 			lock.lock();
