c9f5d2b801e168def79340c07960e9c1f7a831b7
Mauro Trevisan <mauro.simioni@targaubiest.com>
2020-07-28 15:32:15 +0200

    no message

--- src/test/java/io/github/mtrevisan/boxon/codecs/ProtocolMessageParserTest.java
+++ src/test/java/io/github/mtrevisan/boxon/codecs/ProtocolMessageParserTest.java
@@ -24,12 +24,20 @@
  */
 package io.github.mtrevisan.boxon.codecs;
 
+import io.github.mtrevisan.boxon.annotations.BindArray;
+import io.github.mtrevisan.boxon.annotations.BindByte;
+import io.github.mtrevisan.boxon.annotations.BindString;
+import io.github.mtrevisan.boxon.annotations.MessageHeader;
+import io.github.mtrevisan.boxon.annotations.ObjectChoices;
+import io.github.mtrevisan.boxon.annotations.converters.Converter;
 import io.github.mtrevisan.boxon.codecs.queclink.ACKMessageASCII;
 import io.github.mtrevisan.boxon.codecs.queclink.ACKMessageHex;
 import io.github.mtrevisan.boxon.codecs.queclink.DeviceTypes;
+import io.github.mtrevisan.boxon.helpers.BitSet;
 import io.github.mtrevisan.boxon.helpers.ByteHelper;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.expression.spel.SpelEvaluationException;
 
 import java.nio.charset.StandardCharsets;
 
@@ -89,6 +97,120 @@ class ProtocolMessageParserTest{
 		Integer result = Evaluator.evaluate("8/4", Integer.class, null);
 
 		Assertions.assertEquals(2, result);
+	}
+
+
+	@MessageHeader(start = "te1")
+	static class TestError1{
+		@BindString(size = "3")
+		public String header;
+		@BindByte(condition = "e")
+		public byte type;
+	}
+
+	@Test
+	void parseWithConditionError(){
+		byte[] payload = ByteHelper.toByteArray("746335011234");
+		BitReader reader = BitReader.wrap(payload);
+
+		ProtocolMessageParser protocolMessageParser = new ProtocolMessageParser();
+		protocolMessageParser.loader.loadCodecs();
+		ProtocolMessage<TestError1> protocolMessage = ProtocolMessage.createFrom(TestError1.class, protocolMessageParser.loader);
+
+		SpelEvaluationException exc = Assertions.assertThrows(SpelEvaluationException.class, () -> protocolMessageParser.decode(protocolMessage, reader));
+		Assertions.assertEquals("EL1008E: Property or field 'e' cannot be found on object of type 'io.github.mtrevisan.boxon.codecs.ProtocolMessageParserTest$TestError1' - maybe not public or not valid?", exc.getMessage());
+	}
+
+
+	@MessageHeader(start = "te2")
+	static class TestError2{
+		@BindString(size = "3")
+		public String header;
+		@BindByte(match = "as")
+		public byte type;
+	}
+
+	@Test
+	void parseWithMatchError(){
+		byte[] payload = ByteHelper.toByteArray("74633501");
+		BitReader reader = BitReader.wrap(payload);
+
+		ProtocolMessageParser protocolMessageParser = new ProtocolMessageParser();
+		protocolMessageParser.loader.loadCodecs();
+		ProtocolMessage<TestError2> protocolMessage = ProtocolMessage.createFrom(TestError2.class, protocolMessageParser.loader);
+
+		Exception exc = Assertions.assertThrows(IllegalArgumentException.class, () -> protocolMessageParser.decode(protocolMessage, reader));
+		Assertions.assertEquals("Value `1` does not match constraint `as`, field TestError2.type", exc.getMessage());
+	}
+
+
+	@MessageHeader(start = "te3")
+	static class TestError3{
+		class WrongConverter implements Converter<Byte, String>{
+
+			@Override
+			public String decode(final Byte value){
+				return "";
+			}
+
+			@Override
+			public Byte encode(final String value){
+				return null;
+			}
+
+		}
+		@BindString(size = "3")
+		public String header;
+		@BindByte(converter = WrongConverter.class)
+		public byte type;
+	}
+
+	@Test
+	void parseWithConverterOutputError(){
+		byte[] payload = ByteHelper.toByteArray("74633501");
+		BitReader reader = BitReader.wrap(payload);
+
+		ProtocolMessageParser protocolMessageParser = new ProtocolMessageParser();
+		protocolMessageParser.loader.loadCodecs();
+		ProtocolMessage<TestError3> protocolMessage = ProtocolMessage.createFrom(TestError3.class, protocolMessageParser.loader);
+
+		Exception exc = Assertions.assertThrows(IllegalArgumentException.class, () -> protocolMessageParser.decode(protocolMessage, reader));
+		Assertions.assertEquals("Can not set byte field io.github.mtrevisan.boxon.codecs.ProtocolMessageParserTest$TestError3.type to java.lang.String, field TestError3.type", exc.getMessage());
+	}
+
+
+	@MessageHeader(start = "te4")
+	static class TestError4{
+		class WrongConverter implements Converter<String, Byte>{
+
+			@Override
+			public Byte decode(final String value){
+				return null;
+			}
+
+			@Override
+			public String encode(final Byte value){
+				return "";
+			}
+
+		}
+		@BindString(size = "3")
+		public String header;
+		@BindByte(converter = WrongConverter.class)
+		public byte type;
+	}
+
+	@Test
+	void parseWithConverterInputError(){
+		byte[] payload = ByteHelper.toByteArray("74633501");
+		BitReader reader = BitReader.wrap(payload);
+
+		ProtocolMessageParser protocolMessageParser = new ProtocolMessageParser();
+		protocolMessageParser.loader.loadCodecs();
+		ProtocolMessage<TestError4> protocolMessage = ProtocolMessage.createFrom(TestError4.class, protocolMessageParser.loader);
+
+		Exception exc = Assertions.assertThrows(IllegalArgumentException.class, () -> protocolMessageParser.decode(protocolMessage, reader));
+		Assertions.assertEquals("class java.lang.Byte cannot be cast to class java.lang.String (java.lang.Byte and java.lang.String are in module java.base of loader 'bootstrap'), field TestError4.type", exc.getMessage());
 	}
 
 }
