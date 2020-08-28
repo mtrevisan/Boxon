959572b32c92e79555daa1383cd8bd2138d0e53d
Mauro Trevisan <mauro.simioni@targaubiest.com>
2020-08-28 10:19:50 +0200

    no message

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,6 +7,7 @@ module io.github.mtrevisan.boxon{
 	opens io.github.mtrevisan.boxon.codecs to spring.core, spring.expression;
 
 	exports io.github.mtrevisan.boxon.annotations;
+	exports io.github.mtrevisan.boxon.annotations.bindings;
 	exports io.github.mtrevisan.boxon.annotations.checksummers;
 	exports io.github.mtrevisan.boxon.annotations.converters;
 	exports io.github.mtrevisan.boxon.annotations.validators;
