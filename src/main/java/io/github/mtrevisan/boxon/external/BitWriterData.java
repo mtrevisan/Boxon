a19e5c6017a349fdfc1208f7859b7aa51b538247
Mauro Trevisan <mauro.simioni@targaubiest.com>
2020-08-31 14:43:44 +0200

    no message

--- src/main/java/io/github/mtrevisan/boxon/external/BitWriterData.java
+++ src/main/java/io/github/mtrevisan/boxon/external/BitWriterData.java
@@ -51,7 +51,7 @@ class BitWriterData{
 	 * @param value	The value to write.
 	 * @param length	The amount of bits to use when writing {@code value} (MUST BE less than or equals to {@link Long#SIZE}).
 	 */
-	final void putValue(final long value, final int length){
+	public final void putValue(final long value, final int length){
 		final BitSet bits = BitSet.valueOf(new long[]{value});
 		putBits(bits, length);
 	}
