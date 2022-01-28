2ca2f30ff87eb1f0648dd386ad998a3cde73dce9
Mauro Trevisan <mauro.simioni@targaubiest.com>
2022-01-28 15:56:43 +0100

    no message

--- src/test/java/io/github/mtrevisan/boxon/core/ParserTest.java
+++ src/test/java/io/github/mtrevisan/boxon/core/ParserTest.java
@@ -56,8 +56,8 @@ class ParserTest{
 			.withDefaultConfigurations();
 		Parser parser = Parser.create(core);
 
-		//BoxonBitSet: 245-265 µs/msg = 4.1-3.8 kHz
-		//BitSet: 936 µs/msg = 1.1 kHz
+		//BoxonBitSet: 254-261 µs/msg = 3.8-3.9 kHz
+		//BitSet: 248-266 µs/msg = 3.7-4.0 kHz
 		byte[] payload = StringHelper.toByteArray("2b41434b066f2446010a0311235e40035110420600ffff07e30405083639001265b60d0a2b41434b066f2446010a0311235e40035110420600ffff07e30405083639001265b60d0a");
 		//warm-up
 		for(int i = 0; i < 2_000; i ++)
