5e37132c2daebef542c76da06fd505f554d675bb
Mauro Trevisan <mauro.simioni@targaubiest.com>
2022-02-16 12:59:37 +0100

    no message

--- src/test/java/io/github/mtrevisan/boxon/internal/matchers/PatternMatcherTest.java
+++ src/test/java/io/github/mtrevisan/boxon/internal/matchers/PatternMatcherTest.java
@@ -22,7 +22,7 @@
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
  * OTHER DEALINGS IN THE SOFTWARE.
  */
-package io.github.mtrevisan.boxon.internal.codecs.managers.matchers;
+package io.github.mtrevisan.boxon.internal.matchers;
 
 import io.github.mtrevisan.boxon.internal.matchers.BNDMPatternMatcher;
 import io.github.mtrevisan.boxon.internal.matchers.KMPPatternMatcher;
