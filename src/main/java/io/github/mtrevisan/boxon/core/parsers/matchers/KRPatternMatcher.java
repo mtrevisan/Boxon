cbb3ebee50989f68102d36bdb72ae6bf3c47e60f
Mauro Trevisan <mauro.simioni@targaubiest.com>
2022-03-04 12:42:21 +0100

    no message

---
+++ src/main/java/io/github/mtrevisan/boxon/core/parsers/matchers/KRPatternMatcher.java
@@ -0,0 +1,117 @@
+/*
+ * Copyright (c) 2020-2022 Mauro Trevisan
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+package io.github.mtrevisan.boxon.core.parsers.matchers;
+
+
+/**
+ * An implementation of the Karp-Rabin searching algorithm.
+ *
+ * <pre>{@code
+ *  Searching    : Θ(m + n)	(average and best case)
+ *                 O(m · n)	(worst case)
+ * }</pre>
+ */
+public final class KRPatternMatcher implements PatternMatcher{
+
+	private static class SingletonHelper{
+		private static final PatternMatcher INSTANCE = new KRPatternMatcher();
+	}
+
+
+	/**
+	 * Singleton instance of this pattern matcher.
+	 *
+	 * @return	The instance of this pattern matcher.
+	 */
+	public static PatternMatcher getInstance(){
+		return SingletonHelper.INSTANCE;
+	}
+
+
+	private KRPatternMatcher(){}
+
+
+	@Override
+	public int[] preProcessPattern(final byte[] pattern){
+		return null;
+	}
+
+	/**
+	 * Returns the starting position of the first occurrence of the specified pattern array within the specified source array,
+	 * or {@code -1} if there is no such occurrence.
+	 * <p>More formally, returns the lowest index such that {@code source.subArray(i, i + pattern.size()).equals(pattern)},
+	 * or {@code -1} if there is no such index.</p>
+	 * (Returns {@code -1} if {@code pattern.size() > source.size()})
+	 * <p>This implementation uses the "Knuth-Morris-Pratt" technique of scanning over the source list.</p>
+	 *
+	 * @param source	The list in which to search for the first occurrence of {@code pattern}.
+	 * @param offset	Offset to start the search from.
+	 * @param pattern	The list to search for as a subList of {@code source}.
+	 * @param failureTable	Longest Prefix Suffix array precomputed by {@link #preProcessPattern(byte[])}.
+	 * @return	The starting position of the first occurrence of the specified pattern list within the specified source list,
+	 * 	or {@code -1} if there is no such occurrence.
+	 */
+	@Override
+	public int indexOf(final byte[] source, final int offset, final byte[] pattern, final int[] failureTable){
+		if(pattern.length == 0)
+			return 0;
+		if(source.length < pattern.length)
+			return -1;
+
+		//calculate the hash value of pattern and first window of text
+		int patternHash = 0;
+		int sourceHash = 0;
+		for(int i = 0; i < pattern.length; i ++){
+			patternHash = (patternHash << 1) + pattern[i];
+			sourceHash = (sourceHash << 1) + source[i + offset];
+		}
+
+		// check for match at offset 0
+		if(patternHash == sourceHash && equals(source, 0, pattern))
+			return 0;
+
+		for(int i = 0; i < source.length - pattern.length; ){
+			//calculate hash value for next window of text by removing leading digit add trailing digit
+			sourceHash -= source[i] << (pattern.length - 1);
+			sourceHash = (sourceHash << 1) + source[i + pattern.length];
+
+			//check the hash values of current window of source and pattern
+			i ++;
+			if(patternHash == sourceHash && equals(source, i, pattern))
+				return i;
+		}
+
+		return -1;
+	}
+
+	private static boolean equals(final byte[] array1, final int offset, final byte[] array2){
+		int i = 0;
+		for( ; i < array2.length; i ++)
+			if(array1[i + offset] != array2[i])
+				break;
+		return (i == array2.length);
+	}
+
+}
--- src/test/java/io/github/mtrevisan/boxon/core/parsers/matchers/PatternMatcherTest.java
+++ src/test/java/io/github/mtrevisan/boxon/core/parsers/matchers/PatternMatcherTest.java
@@ -24,8 +24,12 @@
  */
 package io.github.mtrevisan.boxon.core.parsers.matchers;
 
+import io.github.mtrevisan.boxon.core.TimeWatch;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 @SuppressWarnings("ALL")
@@ -41,6 +45,39 @@ class PatternMatcherTest{
 	void indexOfKMP(){
 		PatternMatcher pm = KMPPatternMatcher.getInstance();
 		indexOf(pm);
+	}
+
+	@Test
+	void indexOfKR(){
+		PatternMatcher pm = KRPatternMatcher.getInstance();
+		indexOf(pm);
+	}
+
+	@Test
+	void speed(){
+		PatternMatcher bndm = BNDMPatternMatcher.getInstance();
+		PatternMatcher kmp = KMPPatternMatcher.getInstance();
+		PatternMatcher kr = KRPatternMatcher.getInstance();
+		Map<String, PatternMatcher> matchers = new HashMap<>(3);
+		matchers.put("BNDM", bndm);
+		matchers.put("KMP", kmp);
+		matchers.put("KR", kr);
+
+		for(Map.Entry<String, PatternMatcher> entry : matchers.entrySet()){
+			String key = entry.getKey();
+			PatternMatcher m = entry.getValue();
+
+			//warm-up
+			for(int i = 0; i < 2_000; i ++)
+				indexOf(m);
+
+			TimeWatch watch = TimeWatch.start();
+			for(int i = 0; i < 20_000; i ++)
+				indexOf(m);
+			watch.stop();
+
+			System.out.println(key + ": " + watch.toString(20_000));
+		}
 	}
 
 	private void indexOf(PatternMatcher pm){
@@ -96,6 +133,12 @@ class PatternMatcherTest{
 		pattern = "ababcababdabababcababdaba".getBytes();
 		index = pm.indexOf(source, 0, pattern, pm.preProcessPattern(pattern));
 		Assertions.assertEquals(0, index);
+
+
+		source = "2b41434b066f2446010a0311235e40035110420600ffff07e30405083639001265b60d0a2b41434b066f2446010a0311235e40035110420600ffff07e30405083639001265b60d0a".getBytes();
+		pattern = "0d0a".getBytes();
+		index = pm.indexOf(source, 0, pattern, pm.preProcessPattern(pattern));
+		Assertions.assertEquals(68, index);
 	}
 
 }
