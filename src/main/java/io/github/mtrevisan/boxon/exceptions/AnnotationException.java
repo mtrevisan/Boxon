cbc5e1f35be9b8bf32db9abf15aa2a6e342010d3
mauro <851903+mtrevisan@users.noreply.github.com>
2023-01-08 12:49:02 +0100

    library versions

--- src/main/java/io/github/mtrevisan/boxon/exceptions/AnnotationException.java
+++ src/main/java/io/github/mtrevisan/boxon/exceptions/AnnotationException.java
@@ -26,8 +26,6 @@ package io.github.mtrevisan.boxon.exceptions;
 
 import io.github.mtrevisan.boxon.helpers.StringHelper;
 
-import java.io.Serial;
-
 
 /**
  * Thrown if an annotation is not well formatted.
