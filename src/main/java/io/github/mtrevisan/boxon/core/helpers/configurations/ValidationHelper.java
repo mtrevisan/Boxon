4056db6d4ca3d44dcfdcae30eeb1e2adecfe8fea
Mauro Trevisan <mauro.simioni@targatelematics.com>
2024-03-22 09:50:54 +0100

    refactor

--- src/main/java/io/github/mtrevisan/boxon/core/helpers/configurations/ValidationHelper.java
+++ src/main/java/io/github/mtrevisan/boxon/core/helpers/configurations/ValidationHelper.java
@@ -58,23 +58,22 @@ final class ValidationHelper{
 	 */
 	static void validateProtocol(final ConfigFieldData field, final Version minProtocolVersion, final Version maxProtocolVersion)
 			throws AnnotationException{
-		if(StringHelper.isBlank(field.getMinProtocol()) && StringHelper.isBlank(field.getMaxProtocol()))
-			return;
-
 		final Version minimum = validateProtocol(field.getMinProtocol(), field.getAnnotationName(),
 			"Invalid minimum protocol version in {}; found {}");
 		final Version maximum = validateProtocol(field.getMaxProtocol(), field.getAnnotationName(),
 			"Invalid maximum protocol version in {}; found {}");
 
-		//`maxProtocol` must be after or equal to `minProtocol`
-		if(minimum != null && maximum != null && maximum.isLessThan(minimum))
-			throw AnnotationException.create("Minimum protocol version is greater than maximum protocol version in {}; found {}",
-				field.getAnnotationName(), field.getMaxProtocol());
+		if(minimum != null){
+			//`maxProtocol` must be after or equal to `minProtocol`
+			if(maximum != null && maximum.isLessThan(minimum))
+				throw AnnotationException.create("Minimum protocol version is greater than maximum protocol version in {}; found {}",
+					field.getAnnotationName(), field.getMaxProtocol());
 
-		//`minProtocol` must be after or equal to `minProtocolVersion`
-		if(minimum != null && !minProtocolVersion.isEmpty() && minimum.isLessThan(minProtocolVersion))
-			throw AnnotationException.create("Minimum protocol version is less than whole message minimum protocol version in {}; expected {} >= {}",
-				field.getAnnotationName(), minimum, minProtocolVersion);
+			//`minProtocol` must be after or equal to `minProtocolVersion`
+			if(!minProtocolVersion.isEmpty() && minimum.isLessThan(minProtocolVersion))
+				throw AnnotationException.create("Minimum protocol version is less than whole message minimum protocol version in {}; expected {} >= {}",
+					field.getAnnotationName(), minimum, minProtocolVersion);
+		}
 		//`maxProtocol` must be before or equal to `maxProtocolVersion`
 		if(maximum != null && !maxProtocolVersion.isEmpty() && maxProtocolVersion.isLessThan(maximum))
 			throw AnnotationException.create("Maximum protocol version is greater than whole message maximum protocol version in {}; expected {} <= {}",
