b843ccd23e8427162649e051b5f39f20d1ac51ea
Mauro Trevisan <mauro.simioni@targatelematics.com>
2024-10-04 16:34:57 +0200

    Add longDescription field to annotations
    This commit adds a longDescription field with a default empty string to various annotation classes. This enables a more detailed description of fields across BindStringTerminated, BindString, BindBitSet, BindObject, BindInteger, Evaluate, and PostProcess annotations. This helps improve the documentation and usability of these annotations.

--- src/main/java/io/github/mtrevisan/boxon/annotations/bindings/BindObject.java
+++ src/main/java/io/github/mtrevisan/boxon/annotations/bindings/BindObject.java
@@ -114,6 +114,14 @@ public @interface BindObject{
 	 */
 	ConverterChoices selectConverterFrom() default @ConverterChoices;
 
+
+	/**
+	 * A long description of the field.
+	 *
+	 * @return	A long description of the field.
+	 */
+	String longDescription() default "";
+
 	/**
 	 * The unit of measure of the value (the format should follow
 	 * <a href="https://ucum.org/ucum.html">UCUM</a>/<a href="https://en.wikipedia.org/wiki/Unified_Code_for_Units_of_Measure">ISO 80000</a>
